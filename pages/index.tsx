310a45a849eacbf0776e172c90b6dfc2d56ec40b
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2023-03-07 11:40:50 -0500

    added some basic styling and fonts. started navbar rough draft

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
 	return (
@@ -12,7 +13,40 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main></main>
+
+			<main className="bg-white px-10 ">
+				<section className="bg-blue-500 min-h-screen">
+					<nav className="py-10 mb-12 flex justify-between">
+						<h1 className="text-xl">Alex Lee Myers</h1>
+
+						<ul className="flex items-center">
+							<li>
+								<BsFillMoonStarsFill />
+							</li>
+							<li>
+								<a
+									className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+									href="#"
+								>
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#">Resume</a>
+							</li>
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li>
+								<a href="#">Projects</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</nav>
+				</section>
+			</main>
 		</>
 	);
 }
